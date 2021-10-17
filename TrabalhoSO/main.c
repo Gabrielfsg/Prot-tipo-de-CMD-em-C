58d1f1eac9962cf4157ef475d72d71cb22432a01
Gabriel Fernandes <profissional.gabrielg@gmail.com>
2021-10-17 20:47:02 -0300

    Subindo o trabalhono git

---
+++ TrabalhoSO/main.c
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: gabriel f
+ *
+ * Created on 17 de outubro de 2021, 20:39
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
