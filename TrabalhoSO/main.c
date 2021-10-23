67070be95e6f163cc9c619723f4e2178d839d00a
Gabriel Fernandes <profissional.gabrielg@gmail.com>
2021-10-22 22:18:06 -0300

    Verifica se possui pipe e funções auxiliares!

--- TrabalhoSO/main.c
+++ TrabalhoSO/main.c
@@ -14,19 +14,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 #define max 300
-
 
 /*
  * 
  */
 int main(int argc, char** argv) {
     char comando[max];
-    do{
+    bool comandoComPipe = false;
+    printf("Simulador Terminal. Para sair digite (sair) \n");
+    do {
         printf("Digite o comando: ");
         gets(comando);
+        comandoComPipe = verificaComandoPipe(comando);
+        if(comandoComPipe == true){
+            printf("Comando com pipe. \n");
+        } else{
+            printf("Comando sem pipe. \n");
+        }
         printf("\n");
-    } while (strcmp(comando,"sair") != 0);
-    return(0);
+    } while (strcmp(comando, "sair") != 0);
+    return (0);
 }
 
+int verificaComandoPipe(char comando[max]) {
+    bool comandoComPipe = false;
+        if(strstr(comando, "|")){
+            comandoComPipe = true;
+            return comandoComPipe;
+        } else {
+            return comandoComPipe;
+        }
+}
+
+int contaCaracteres(char string[max]) {
+    int cont = strlen(string);
+    return cont;
+}
