4e8437dd5bf775e162893f8a5ddeaae7522c19cf
delasmancha <delasmancha@gmail.com>
2021-10-23 23:53:39 -0300

    executa o comando sem pipe em um fork!

--- TrabalhoSO/main.c
+++ TrabalhoSO/main.c
@@ -10,12 +10,34 @@
  *
  * Created on 17 de outubro de 2021, 20:39
  */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <wait.h>
 #define max 300
+
+void execv_comando(char comando[max]){
+    pid_t pid;
+    pid = fork();
+    if(pid<0){
+        perror("Erro na criacao do processo filho\n");
+        exit(1);
+    }
+    if(pid == 0){
+        int ret;
+        char copia[max];
+        strcpy(copia, comando);
+        char *token = strtok(comando, " ");
+        execlp(token, copia, (char *) NULL);
+    }
+    if(pid > 0){
+        wait(NULL);
+        return;
+    }
+}
 
 /*
  * 
@@ -32,6 +54,7 @@ int main(int argc, char** argv) {
             printf("Comando com pipe. \n");
         } else{
             printf("Comando sem pipe. \n");
+            execv_comando(comando);
         }
         printf("\n");
     } while (strcmp(comando, "sair") != 0);
