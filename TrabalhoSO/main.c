af52d884f1463ddb83418353e72e8148e4dc0f62
Gabriel Fernandes <profissional.gabrielg@gmail.com>
2021-11-03 22:34:09 -0300

    Correções

--- TrabalhoSO/main.c
+++ TrabalhoSO/main.c
@@ -17,6 +17,7 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <wait.h>
+#include <assert.h>
 #define max 300
 #define READ_END 0
 #define WRITE_END 1
@@ -32,7 +33,30 @@ void execv_comando(char comando[max]) {
         char copia[max];
         strcpy(copia, comando);
         char *token = strtok(copia, " ");
-        execlp(token, comando, (char *) NULL);
+        printf("Comando: %s\n", comando);
+        printf("copia: %s\n", copia);
+        printf("token: %s\n", token);
+
+        char** result = 0;
+        char* temp = 0;
+        int tamanho = 0;
+        temp = strtok(comando, " ");
+        if (temp) {
+            result = malloc((tamanho + 1) * sizeof (char**));
+            result[tamanho++] = temp;
+        }
+        while ((temp = strtok(0, "|")) != 0) {
+            result = realloc(result, (tamanho + 1) * sizeof (char**));
+            result[tamanho++] = temp;
+        }
+        for (int i = 0; i < tamanho; i++) {
+            printf("%d - %s\n", i, result[i]);
+        }
+        if (tamanho == 1) {
+            execlp(result[0], result[0], (char *) NULL);
+        } else {
+            execlp(result[0], result[0], result[1], 0);
+        }
     }
     if (pid > 0) {
         wait(NULL);
@@ -53,47 +77,103 @@ void execpipe(char comando[max]) {
         result = realloc(result, (tamanho + 1) * sizeof (char**));
         result[tamanho++] = temp;
     }
+
+
     for (int i = 0; i < tamanho; i++) {
         printf("%d - %s\n", i, result[i]);
     }
-    int numeroDePipes = tamanho - 1;
-    int des_p[2];
+
+
+//    printf("tamanho: %d \n",tamanho);
+//    int numeroDePipes = tamanho - 1;
+//    
+//    int des_p[2];
 //    if (pipe(des_p) == -1) {
 //        perror("Falha na criacao do pipe");
 //        exit(1);
 //    }
-//    for (int i = 0; i < tamanho; i++) {
-//        if (i == 0) {
-//            if (fork() == 0) {
-//                char *comandoSemArgumento = strtok(result[i], " ");
+    for (int i = 0; i < tamanho; i++) {
+//
+//        if (fork() == 0) {
+//            printf("%d etapa. \n", i);
+//            char comandoSemArgumento[max];
+//            printf("%s comando. \n", result[i]);
+//            strcpy(comandoSemArgumento, result[i]);
+//            char** result2 = 0;
+//            char* temp2 = 0;
+//            int tamanho2 = 0;
+//            temp2 = strtok(comandoSemArgumento, " ");
+//            if (temp2) {
+//                result2 = malloc((tamanho2 + 1) * sizeof (char**));
+//                result2[tamanho2++] = temp2;
+//            }
+//            while ((temp2 = strtok(0, " ")) != 0) {
+//                result2 = realloc(result2, (tamanho2 + 1) * sizeof (char**));
+//                result2[tamanho2++] = temp2;
+//            }
+//            
+//            if (i == 0) {
 //                close(STDOUT_FILENO);
 //                dup(des_p[WRITE_END]);
 //                close(des_p[WRITE_END]);
 //                close(des_p[READ_END]);
-//                execlp(comandoSemArgumento, result[i], (char *) NULL);
-//                perror("Falha ao executar a primeira parte do comando");
-//            }
-//        } else {
-//            if (fork() == 0) {
-//                char *comandoSemArgumento = strtok(result[i], " ");
+//            } else if (i = tamanho - 1) {
+//                close(STDIN_FILENO);
+//                dup(des_p[READ_END]);
+//                close(des_p[READ_END]);
+//                close(des_p[READ_END]);
+//            } else {
 //                close(STDIN_FILENO);
 //                dup(des_p[READ_END]);
 //                dup(des_p[WRITE_END]);
 //                close(des_p[READ_END]);
 //                close(des_p[READ_END]);
-//                execlp(comandoSemArgumento, result[i], (char *) NULL);
-//                perror("Falha ao executar as outras partes do comando");
-//                exit(1); //se o exec der errado, fecha o processo filho B pois nÃ£o faz sentido continuar
-//                  if(i == numeroDePipes){
-//                     close(des_p[WRITE_END]);
+//            }
+//            if (tamanho2 == 1) {
+//                execlp(result2[0], result2[0], (char *) NULL);
+//            } else {
+//                execlp(result[0], result[0], result[1], 0);
+//            }
+//            printf("Falha ao executar o %d ° comando", i);
+//        }
+//
+//
+//        if (i == tamanho - 1) {
+//            close(des_p[WRITE_END]);
 //            close(des_p[READ_END]);
 //            wait(NULL);
 //            wait(NULL);
-//                  }
-//            }
 //        }
-//    }
+    }
 }
+
+void pipeline(char ***cmd) {
+    int fd[2];
+    pid_t pid;
+    int fdd = 0;
+
+    while (*cmd != NULL) {
+        pipe(fd);
+        if ((pid = fork()) == -1) {
+            perror("fork");
+            exit(1);
+        } else if (pid == 0) {
+            dup2(fdd, 0);
+            if (*(cmd + 1) != NULL) {
+                dup2(fd[1], 1);
+            }
+            close(fd[0]);
+            execvp((*cmd)[0], *cmd);
+            exit(1);
+        } else {
+            wait(NULL);
+            close(fd[1]);
+            fdd = fd[0];
+            cmd++;
+        }
+    }
+}
+
 /*
  * 
  */
@@ -110,7 +190,7 @@ int main(int argc, char** argv) {
             execpipe(comando);
         } else {
             printf("Comando sem pipe. \n");
-            //execv_comando(comando);
+            execv_comando(comando);
         }
         printf("\n");
     } while (strcmp(comando, "sair") != 0);
@@ -132,37 +212,3 @@ int contaCaracteres(char string[max]) {
     return cont;
 }
 
-
-
-// EXEMPLOS:
-
-//        if (fork() == 0){
-//            close(STDOUT_FILENO);
-//            dup(des_p[WRITE_END]);
-//            close(des_p[WRITE_END]);
-//            close(des_p[READ_END]);
-//            execlp(copia, comando1, (char *) NULL);
-//            perror("Falha ao executar a primeira parte do comando");
-//            exit(1);
-//        }
-//    
-//        if (fork() == 0){
-//close(STDIN_FILENO);
-//dup(des_p[READ_END]);
-//close(des_p[READ_END]);
-//close(des_p[WRITE_END]);
-//
-//char *b = strtok(comando2, "|");
-//strcpy(copia, comando2);
-//char *c = strtok(copia, " ");
-//execlp(copia, comando2, (char *) NULL);
-//perror("Falha ao executar as outras partes do comando");
-//exit(1); //se o exec der errado, fecha o processo filho B pois nÃ£o faz sentido continuar
-//        }
-// else{
-//            close(des_p[WRITE_END]);
-//            close(des_p[READ_END]);
-//            wait(NULL);
-//            wait(NULL);
-//        }
-//}
